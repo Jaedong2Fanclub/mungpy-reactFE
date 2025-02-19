0869bc1b3aa515d6b6d94bcc480cf15dc35c4273
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-02-20 01:29:10 +0900

    fix: Rename folder to lowercase

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MainPage from '../component/main/mungpy/main'
+import mainPage from '../component/main/mungpy/main'
 import Mungpy_Main from "../component/main/mungpy/mungpy_main";
 const Main = () => {
     return (
